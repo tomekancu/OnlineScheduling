35d9dd6a18be472ee964777492c30d3267b8cf14
Tomasz_Andrzej Ancukiewicz <tomasz_andrzej.ancukiewicz@student.put.poznan.pl>
2020-01-26 13:53:53 +0100

    fixes to research

--- research.py
+++ research.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from typing import Dict, Any, Tuple, List
+from typing import Any, List
 
 from cost_functions import LengthFunctionType
 from generator import Generator
@@ -20,51 +20,60 @@ class Variable(Enum):
     LENGTH_FUNCTION = auto()
 
 
-def make_research(default: Dict[Variable, Any], testing: Tuple[Variable, List[Any]]):
-    n_procesors = default.get(Variable.N_PROCESORS, 100)
-    task_number = default.get(Variable.TASK_NUMBER, 10_000)
-    max_load = default.get(Variable.MAX_LOAD, 0.2)
-    cov = default.get(Variable.COV, 0.3)
-    max_part_of_processors = default.get(Variable.MAX_PART_OF_PROCESOR_NUMBER, 1.0)
-    length_function = default.get(Variable.LENGTH_FUNCTION, LengthFunctionType.CONCAVE)
-    testing_type, testing_values = testing
+class Parameters:
+    def __init__(self, test_number: int, n_procesors: int, task_number: int, max_load: float, cov: float,
+                 max_part_of_processors: float, length_function: LengthFunctionType):
+        self.test_number = test_number
+        self.n_procesors = n_procesors
+        self.task_number = task_number
+        self.max_load = max_load
+        self.cov = cov
+        self.max_part_of_processors = max_part_of_processors
+        self.length_function = length_function
 
-    metrics_all = {}
-    print(f"n_procesors={n_procesors}, task_number={task_number}")
-    print(f"max_load={max_load}, cov={cov}")
-    print(f"max_part_of_processors={max_part_of_processors}, length_function={length_function}")
-    print(f"testing_type={testing_type}, testing_values={testing_values}")
-    for vals in testing_values:
-        print(f"testing: {vals}")
+    def update(self, testing_type: Variable, val: Any):
         if testing_type == Variable.N_PROCESORS:
-            n_procesors = vals
+            self.n_procesors = val
         elif testing_type == Variable.TASK_NUMBER:
-            task_number = vals
+            self.task_number = val
         elif testing_type == Variable.MAX_LOAD:
-            max_load = vals
+            self.max_load = val
         elif testing_type == Variable.COV:
-            cov = vals
+            self.cov = val
         elif testing_type == Variable.MAX_PART_OF_PROCESOR_NUMBER:
-            max_part_of_processors = vals
+            self.max_part_of_processors = val
         elif testing_type == Variable.LENGTH_FUNCTION:
-            length_function = vals
+            self.length_function = val
 
-        gen = Generator(task_number=task_number, processors_number=n_procesors,
-                        coefficient_of_variation=cov, max_load=max_load,
-                        length_function=length_function.get_function(),
-                        max_part_of_processors_number=max_part_of_processors,
+    def print(self):
+        print(f"n_procesors={self.n_procesors}, task_number={self.task_number}")
+        print(f"max_load={self.max_load}, cov={self.cov}")
+        print(f"max_part_of_processors={self.max_part_of_processors}, length_function={self.length_function}")
+
+
+def make_research(params: Parameters, testing_type: Variable, testing_values: List[Any]):
+    params.print()
+    print(f"testing_type={testing_type}, testing_values={testing_values}")
+    metrics_all = {}
+    for val in testing_values:
+        print(f"testing: {val}")
+        params.update(testing_type, val)
+        gen = Generator(task_number=params.task_number, processors_number=params.n_procesors,
+                        coefficient_of_variation=params.cov, max_load=params.max_load,
+                        length_function=params.length_function.get_function(),
+                        max_part_of_processors_number=params.max_part_of_processors,
                         print_plots=False)
         instance = gen.generate()
         metr = {}
 
         print("first")
         scheduler1 = NaiveScheduler()
-        scheduler1.schedule(n_procesors, instance)
+        scheduler1.schedule(params.n_procesors, instance)
         metr[scheduler1.get_name()] = get_metrics(scheduler1.procesors)
 
         print("second25")
         scheduler2 = SeparateScheduler(gen.get_mid_task_size(), 0.25)
-        scheduler2.schedule(n_procesors, instance)
+        scheduler2.schedule(params.n_procesors, instance)
         metr[scheduler2.get_name()] = get_metrics(scheduler2.procesors)
 
         # print("second50")
@@ -74,33 +83,34 @@ def make_research(default: Dict[Variable, Any], testing: Tuple[Variable, List[An
 
         print("second25v2")
         scheduler4 = SeparateWithPremptionScheduler(gen.get_mid_task_size(), 0.25)
-        scheduler4.schedule(n_procesors, instance)
+        scheduler4.schedule(params.n_procesors, instance)
         metr[scheduler4.get_name()] = get_metrics(scheduler4.procesors)
 
         print("second50v2")
         scheduler5 = SeparateWithPremptionScheduler(gen.get_mid_task_size(), 0.5)
-        scheduler5.schedule(n_procesors, instance)
+        scheduler5.schedule(params.n_procesors, instance)
         metr[scheduler5.get_name()] = get_metrics(scheduler5.procesors)
 
         print("third")
         scheduler6 = PreemptionScheduler()
-        scheduler6.schedule(n_procesors, instance)
+        scheduler6.schedule(params.n_procesors, instance)
         metr[scheduler6.get_name()] = get_metrics(scheduler6.procesors)
 
-        metrics_all[cov] = metr
+        metrics_all[val] = metr
     print_metrics(metrics_all,
-                  f"metrics max_load{max_load} cov{cov} max_part_of_processors{max_part_of_processors} "
-                  f"length_function{length_function} {testing_type}",
-                  f"metrics-max-load-{max_load}-cov-{cov}-max_part_of_processors-{max_part_of_processors}"
-                  f"-length_function-{length_function}-{testing_type}.png")
+                  f"metrics max_load{params.max_load} cov{params.cov} max_part_of_processors{params.max_part_of_processors} "
+                  f"length_function{params.length_function} {testing_type}",
+                  f"metrics-max-load-{params.max_load}-cov-{params.cov}-max_part_of_processors-{params.max_part_of_processors}"
+                  f"-length_function-{params.length_function}-{testing_type}.png")
 
 
 def research():
-    make_research({
-        Variable.TASK_NUMBER: 10_000,
-        Variable.N_PROCESORS: 100,
-        Variable.MAX_LOAD: 0.2,
-        Variable.COV: 0.3,
-        Variable.LENGTH_FUNCTION: LengthFunctionType.CONCAVE,
-        Variable.MAX_PART_OF_PROCESOR_NUMBER: 1.0,
-    }, (Variable.COV, [0.3, 1, 2, 5, 10]))
+    make_research(Parameters(
+        test_number=1,
+        n_procesors=100,
+        task_number=10_000,
+        max_load=0.2,
+        cov=0.3,
+        max_part_of_processors=1.0,
+        length_function=LengthFunctionType.CONCAVE
+    ), Variable.COV, [0.3, 1, 2, 5, 10])
