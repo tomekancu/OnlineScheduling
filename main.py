17dd908811fd7a03047a6d193950302aad1b9cff
Tomasz_Andrzej Ancukiewicz <tomasz_andrzej.ancukiewicz@student.put.poznan.pl>
2020-03-15 09:11:35 +0100

    parametrized main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import sys
 from research import research
 from test import test
 
 if __name__ == '__main__':
     print("Start")
-    # test()
-    research()
+    mode = "t"
+    if len(sys.argv) > 1 and sys.argv[1]:
+        mode = sys.argv[1]
+    if mode == "t":
+        print("Start tests")
+        test()
+    else:
+        print("Start research")
+        research()
